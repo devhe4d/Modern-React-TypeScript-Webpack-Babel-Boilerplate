3c672763b7b5aed09f9217ad6469a1251c959394
Shahryar Rp <shahryar.rajabpoor@gmail.com>
2022-01-11 15:28:14 +0100

    improv 💡- separate bundling npm packages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -34,11 +34,22 @@ module.exports = {
     moduleIds: 'deterministic',
     runtimeChunk: 'single',
     splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1]
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`
+          },
         },
       },
     },
